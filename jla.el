59dd5985b764a956cb07c7f6535cc050f5901d35
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2025-08-03 13:33:35 +0200

    Ficheiro con variables e funcións persoais.

---
+++ jla.el
@@ -0,0 +1,13 @@
+;;; jla.el --- Personal custom variables and functions -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(defgroup jla()
+  "Personal custom variables and functions.")
+
+(defcustom jla/hunspell-dictionaries "gl_ES,es_ES,en_GB"
+  "Comma separated list of Hunspell dictionaries."
+  :group 'jla
+  :type 'list)
+
+(provide 'jla)
+
+;;; jla.el ends here
--- pre-early-init.el
+++ pre-early-init.el
@@ -3,6 +3,19 @@
 ;; UI configurations
 (setq minimal-emacs-ui-features '(context-menu menu-bar dialogs tooltips))
 
+(defun maybe-create-symlink-relative (filename link)
+  "Crea unha ligazón simbólica de FILENAME no directorio actual a LINK.
+FILENAME: O nome do ficheiro a ligar no directorio actual.
+LINK: O destino da ligazón simbólica."
+  (let ((target (concat default-directory filename))
+        (link-dir (file-name-directory link)))
+    (unless (file-exists-p link)
+      (unless (file-exists-p link-dir)
+        (make-directory link-dir t))  ; Crea os directorios pais se non existen.
+      (make-symbolic-link target link))))
+
+(maybe-create-symlink-relative "jla.el" (concat (expand-file-name "jla/" user-emacs-directory) "jla.el"))
+
 ;;; Reducing clutter in ~/.emacs.d by redirecting files to ~/.emacs.d/var/
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
