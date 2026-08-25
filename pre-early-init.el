14ffddbbfedce33af5239d8d9cacb746533a4cfb
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2026-08-25 19:36:56 +0200

    Crear ficheiro pre-early.init.el

---
+++ pre-early-init.el
@@ -0,0 +1,12 @@
+;;; pre-early-init.el --- Configuracións ao inicio do early-ini -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Este ficheiro cárgase tras a definición de varias funcións e variables ao
+;; inicio do `early-init-file'.
+
+;;; Code:
+
+
+
+;;; pre-early-init.el ends here
