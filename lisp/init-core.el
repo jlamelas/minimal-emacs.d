50506619d7bbd5d53cce9667a7129614c7a7194d
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2026-08-27 23:45:35 +0200

    Engadir undo-fu.

--- lisp/init-core.el
+++ lisp/init-core.el
@@ -148,6 +148,24 @@
   ;; editing experience without affecting cursor placement.
   (stripspace-restore-column t))
 
+;; The undo-fu package is a lightweight wrapper around Emacs' built-in undo
+;; system, providing more convenient undo/redo functionality.
+(use-package undo-fu
+  :commands (undo-fu-only-undo
+             undo-fu-only-redo
+             undo-fu-only-redo-all
+             undo-fu-disable-checkpoint)
+  :init
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z") 'undo-fu-only-undo)
+  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
+
+;; The undo-fu-session package complements undo-fu by enabling the saving
+;; and restoration of undo history across Emacs sessions, even after restarting.
+(use-package undo-fu-session
+  :init
+  (undo-fu-session-global-mode 1))
+
 ;; This automates the process of updating installed packages
 (use-package auto-package-update
   :ensure t
