cfaf1d5d077b82cfd6c6f963d7ed2d459957f1dc
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2026-08-27 17:48:50 +0200

    Engadir ficheiro post-init.el ao repositorio.

---
+++ post-init.el
@@ -0,0 +1,17 @@
+;;; post-init-el --- Configuracións post init -*- no-byte-compile: t; lexical-binding: t; -*-
+
+;;;; Commentary:
+
+;; Configuracións post ficheiro init.el
+
+;;; Code:
+
+;; Cargamos ficheiros de configuración
+(condition-case err
+    (progn
+      (require 'init-ui))
+  (error
+   (message "Erro ao cargar o ficheiro %s" err)))
+
+
+;;; post-init.el ends here
