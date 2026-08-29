98859a7b2dc3254e4d0d0e71f11c101f82841891
Juan Lamelas Alfonso <jam.lamelas@gmail.com>
2026-08-29 12:07:23 +0200

    Configuracións para org-mode e Denote.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -6,7 +6,184 @@
 
 ;;; Code:
 
+;; Org mode
+(use-package org
+  :custom
+  (org-startup-indented t)              ; Contido indentado ao iniciar
+  (org-hide-emphasis-markers t)         ; Ocultar marcado
+  (org-startup-with-inline-images t)    ; Mostrar imaxes en liña
+  (org-image-actual-width '(450))       ; Tamaño das imaxes
+  (org-pretty-entities t)               ; Mostrar entidades en UTF-8
+  (org-use-sub-superscripts "{}")       ; Sub-índices e súper-índices
+  (org-id-link-to-org-use-id t)         ; Usar IDs en org-store link
+  (org-startup-folded 'content)         ; Mostrar encabezados e sub-encabezados pero non o contido
+  (org-fold-catch-invisible-edits 'show); Edicións en rexións non visibles fan visible o punteiro e realizan a edición
+  ;; Opcións de exportación
+  (org-export-with-drawers nil)         ; Non exportar gabetas
+  (org-export-with-todo-keywords nil)   ; Non exportar palabras clave
+  (org-export-with-toc nil)             ; Non exportar táboa de contidos
+  (org-export-with-smart-quotes t)      ; Aspas intelixentes
+  (org-export-date-timestamp-format "%e %B %Y") ; Formato de data
+  ;; org-agenda
+  (org-agenda-custom-commands
+   '(("e" "Agenda, next actions and waiting"
+      ((agenda "" ((org-agenda-overriding-header "Next three days:")
+                   (org-agenda-span 3)
+                   (org-agenda-start-on-weekday nil)))
+       (todo "NEXT" ((org-agenda-overriding-header "Next Actions:")))
+       (todo "WAIT" ((org-agenda-overriding-header "Waiting:")))))))
+  ;; Palabras clave
+  (org-todo-keywords '((sequence "NEXT(n)" "TODO(t)" "WAITING(w)" "SOMEDAY(s)" "PROJ(p)"
+				                 "|" "DONE(d)" "CANCELLED(c)")))
+  :bind
+  (("C-c a" . org-agenda)))
 
-(provide 'org-init)
+;; Show hidden emphasis markers
+(use-package org-appear
+  :hook
+  (org-mode . org-appear-mode))
+
+;; Org-capture
+(use-package org
+  :bind
+  (("C-c c" . org-capture)
+   ("C-c l" . org-store-link))
+  :custom
+  (org-goto-interface 'outline-path-completion)
+  (org-capture-templates
+   '(("f" "Fleeting note"
+      item
+      (file+headline org-default-notes-file "Notes")
+      "- %?")
+     ("p" "Permanent note" plain
+      (file denote-last-path)
+      #'denote-org-capture
+      :no-save t
+      :immediate-finish nil
+      :kill-buffer t
+      :jump-to-captured t)
+     ("t" "New task" entry
+      (file+headline org-default-notes-file "Tasks")
+      "* TODO %i%?")
+     ("p" "Protocol" entry (file+headline org-default-notes-file "Inbox")
+      "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
+     ("L" "Protocol Link" entry (file+headline org-default-notes-file "Inbox")
+      "* %? [[%:link][%:description]] \nCaptured On: %U"))))
+
+;; epub export
+(use-package ox-epub
+  :demand t
+  :init
+  (require 'ox-org))
+
+;; LaTeX PDF Export settings
+(use-package ox-latex
+  :demand t
+  :custom
+  ;; Multiple LaTeX passes for bibliographies
+  (org-latex-pdf-process
+   '("pdflatex -interaction nonstopmode -output-directory %o %f"
+     "bibtex %b"
+     "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+     "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+  ;; Clean temporary files after export
+  (org-latex-logfiles-extensions
+   (quote ("lof" "lot" "tex~" "aux" "idx" "log" "out"
+           "toc" "nav" "snm" "vrb" "dvi" "fdb_latexmk"
+           "blg" "brf" "fls" "entoc" "ps" "spl" "bbl"
+           "tex" "bcf"))))
+
+;;Denote
+(use-package denote
+  :hook
+  ( ;; If you use Markdown or plain text files, then you want to make
+   ;; the Denote links clickable (Org renders links as buttons right
+   ;; away)
+   (text-mode . denote-fontify-links-mode-maybe)
+   ;; Apply colours to Denote names in Dired.  This applies to all
+   ;; directories.  Check `denote-dired-directories' for the specific
+   ;; directories you may prefer instead.  Then, instead of
+   ;; `denote-dired-mode', use `denote-dired-mode-in-directories'.
+   (dired-mode . denote-dired-mode))
+  :bind
+  ;; Denote DOES NOT define any key bindings.  This is for the user to
+  ;; decide.
+  ( :map global-map
+    ("C-c n n" . denote)
+    ("C-c n d" . denote-dired)
+    ("C-c n g" . denote-grep)
+    ;; If you intend to use Denote with a variety of file types, it is
+    ;; easier to bind the link-related commands to the `global-map', as
+    ;; shown here.  Otherwise follow the same pattern for `org-mode-map',
+    ;; `markdown-mode-map', and/or `text-mode-map'.
+    ("C-c n l" . denote-link)
+    ("C-c n L" . denote-add-links)
+    ("C-c n b" . denote-backlinks)
+    ("C-c n q c" . denote-query-contents-link) ; create link that triggers a grep
+    ("C-c n q f" . denote-query-filenames-link) ; create link that triggers a dired
+    ;; Note that `denote-rename-file' can work from any context, not just
+    ;; Dired bufffers.  That is why we bind it here to the `global-map'.
+    ("C-c n r" . denote-rename-file)
+    ("C-c n R" . denote-rename-file-using-front-matter)
+
+    ;; Key bindings specifically for Dired.
+    :map dired-mode-map
+    ("C-c C-d C-i" . denote-dired-link-marked-notes)
+    ("C-c C-d C-r" . denote-dired-rename-files)
+    ("C-c C-d C-k" . denote-dired-rename-marked-files-with-keywords)
+    ("C-c C-d C-R" . denote-dired-rename-marked-files-using-front-matter))
+
+  :config
+  ;; Remember to check the doc string of each of those variables.
+  (setq denote-directory (expand-file-name "~/Documents/notes/"))
+  (setq denote-save-buffers nil)
+  (setq denote-infer-keywords t)
+  (setq denote-sort-keywords t)
+  (setq denote-prompts '(title keywords))
+  (setq denote-excluded-directories-regexp nil)
+  (setq denote-excluded-keywords-regexp nil)
+  (setq denote-rename-confirmations '(rewrite-front-matter modify-file-name))
+
+  ;; Pick dates, where relevant, with Org's advanced interface:
+  (setq denote-date-prompt-use-org-read-date t)
+
+  ;; Automatically rename Denote buffers using the `denote-rename-buffer-format'.
+  (denote-rename-buffer-mode 1))
+
+(use-package denote-journal
+  :ensure t
+  ;; Bind those to some key for your convenience.
+  :commands ( denote-journal-new-entry
+              denote-journal-new-or-existing-entry
+              denote-journal-link-or-create-entry )
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :config
+  ;; Use the "journal" subdirectory of the `denote-directory'.  Set this
+  ;; to nil to use the `denote-directory' instead.
+  (setq denote-journal-directory
+        (expand-file-name "journal" denote-directory))
+  ;; Default keyword for new journal entries. It can also be a list of
+  ;; strings.
+  (setq denote-journal-keyword "journal")
+  ;; Read the doc string of `denote-journal-title-format'.
+  (setq denote-journal-title-format 'day-date-month-year))
+
+;; The markdown-mode package provides a major mode for Emacs for syntax
+;; highlighting, editing commands, and preview support for Markdown documents.
+;; It supports core Markdown syntax as well as extensions like GitHub Flavored
+;; Markdown (GFM).
+(use-package markdown-mode
+  :commands (gfm-mode
+             gfm-view-mode
+             markdown-mode
+             markdown-view-mode)
+  :mode (("\\.markdown\\'" . markdown-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("README\\.md\\'" . gfm-mode))
+  :bind
+  (:map markdown-mode-map
+        ("C-c C-e" . markdown-do)))
+
+(provide 'init-org)
 
 ;;; init-org.el ends here
